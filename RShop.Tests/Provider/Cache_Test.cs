3284bb0aae3c58cff47e9c8975e8f7e3b5874cbc
Rocher <Rocher@126.com>
2016-04-25 21:23:36 +0800

    Redis provider test

---
+++ RShop.Tests/Provider/Cache_Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using RShop.Infrastructure.Cache.Interface;
+
+namespace RShop.Tests.Provider
+{
+    [TestClass]
+    public class Cache_Test
+    {
+       public CacheProvider CacheProvider
+        {
+            get {
+                return CacheProvider.LoadInstance<CacheProvider>("RedisCacheProvider");
+            }
+        }
+
+        [TestMethod]
+        public void Cache_AddTest()
+        {
+            string token = Guid.NewGuid().ToString("N");
+            bool isSuccess = CacheProvider.Add<String>(String.Format("Token:{0}",token),"孔翔的token");
+            Assert.IsTrue(isSuccess);
+        }
+    }
+}
